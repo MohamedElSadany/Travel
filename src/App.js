eda16c83dff7bf64e26d658c15074209470f14b2
mohamed.ElSadany <me2929977@gmail.com>
2023-03-12 15:58:12 +0200

    adding resturant name

--- src/App.js
+++ src/App.js
@@ -8,13 +8,22 @@ import Map from './components/Map/Map';
 
 const App = () => {
    const [places,setPlaces] = useState([]);
+   const [coordinates,setCoordinates] = useState({});
+   const [bounds,setBounds] = useState({});
+
+   useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      setCoordinates({ lat: latitude, lng: longitude });
+    });
+  }, []);
+
   useEffect(()=>{
-   getPlacesData()
+   getPlacesData(bounds.sw,bounds.ne)
    .then((data)=>{
     console.log(data);
      setPlaces(data);
    })
-  },[])
+  },[coordinates,bounds])
   return (
     <>
     <CssBaseline />
@@ -22,12 +31,14 @@ const App = () => {
     <Grid container spacing={3} style={{ width: '100%' }}>
       <Grid item xs={12} md={4}>
         <List
-          
+          places={places}
         />
       </Grid>
       <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <Map
-          
+          setCoordinates={setCoordinates}
+          setBounds={setBounds}
+          coordinates={coordinates}
         />
       </Grid>
     </Grid>
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,15 +4,11 @@ import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
 import useStyles from './styles.js';
 
-const List = () => {
+const List = ({places}) => {
   const classes = useStyles();
   const [type,setType] = useState('restaurants');
   const [rating,setRating] = useState('');
-  const places = [
-    {name: 'Cool Place'},
-    {name: 'Best Place'},
-    {name: 'Hot Place'},
-  ];
+ 
   return (
     <div className={classes.container}>
       <Typography variant="h4">Food & Dining around you</Typography>
